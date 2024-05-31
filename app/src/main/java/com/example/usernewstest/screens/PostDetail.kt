a70a90d4fa28af430439a3099dc815592ebb66b4
lucasemiliani14 <lucasemiliani1@hotmail.com>
2024-05-31 14:24:56 -0300

    .

--- app/src/main/java/com/example/usernewstest/screens/PostDetail.kt
+++ app/src/main/java/com/example/usernewstest/screens/PostDetail.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -43,7 +44,7 @@ fun BodyContent(navController: NavController, title: String?, content: String?) 
     {
         TopAppBar({
             Row {
-                Icon(imageVector = Icons.Default.ArrowBack,
+                Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                     contentDescription = "Arrow Back",
                     modifier = Modifier.clickable {
                         navController.popBackStack()
